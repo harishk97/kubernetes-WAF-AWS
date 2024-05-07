c15893e37a04cdd9579a4ec5fd1bf4c7dfbe68f1
Haarish K <harishkalyan200@gmail.com>
2024-05-07 13:11:45 -0300

    named worker node

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "worker" {
     ami = "ami-0c4596ce1e7ae3e68"
     instance_type = "t2.micro"
     tags = {
-      Name = "Worker_Node"
+      Name = "Worker_Node-1"
     }
   
 }
